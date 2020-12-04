f4c5596420f37c1b4f9859ad6fe330e887fa0dec
Ramon Echeverria <ramon.l.echeverria@gmail.com>
2020-12-03 21:57:53 -0500

    updates

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Banner() {
     return (
         <div className="banner">
-            <img className="banner-image" src={"https://cdn.pixabay.com/photo/2016/01/22/08/17/banner-1155437_960_720.png"} alt="Movie Banner"/>
+            <img className="banner-image" src={"https://previews.123rf.com/images/tul/tul1706/tul170600033/79991409-movie-vector-trendy-banner-design-concept-modern-style-with-thin-line-art-icons-on-gradient-colors-b.jpg"} alt="Movie Banner"/>
         </div>
     )
 }
